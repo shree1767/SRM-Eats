907ee518e36ff7db0afd583c4b3bce1c55d928c3
shreenidhi <shreenidhi1757@gmail.com>
2023-07-10 12:06:11 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use( (req,res,next) =>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000","https://srm-eats.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin","https://srm-eats.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
