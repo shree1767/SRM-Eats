944045772d2c0b8fb75a387e145333b160e61348
shreenidhi <shreenidhi1757@gmail.com>
2023-08-02 13:47:05 +0530

    auth role

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
+    role:{
+        type:String,
+        required:true
+    },
     name:{
         type: String,
         required:true
